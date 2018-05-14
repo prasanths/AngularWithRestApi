ba6a16544edad72a2b84911fb865d6aa2d018aa5
prasanths <prasanth.mvk@gmail.com>
2018-05-15 02:41:51 +0530

    Pushing sample application with API call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+
+import { PeopleListComponent } from './components/people.component';
+import { PeopleService } from './components/people.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
